cb084bdd47cf89db9e0dd29d0ad7fac5bd681cb0
atonyxu <atonyxu@163.com>
2026-06-15 00:21:25 +0800

    actions build

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+if not getattr(sys, "frozen", False):
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
 from textgrid_labeler import TextGridLabeler
 
